a965d10ed29310a7253cf06b6a2835e275cfc061
fllesser <19550518zrf@gmail.com>
2024-10-09 18:06:33 +0800

    设置群头衔，sgst触发

--- logic/custom_logic.go
+++ logic/custom_logic.go
@@ -2,28 +2,34 @@ package logic
 
 import (
 	"github.com/LagrangeDev/LagrangeGo/client"
-	"github.com/LagrangeDev/LagrangeGo/client/event"
+	"github.com/LagrangeDev/LagrangeGo/client/entity"
 	"github.com/LagrangeDev/LagrangeGo/message"
 	"github.com/sirupsen/logrus"
+	"strings"
 )
 
 // RegisterCustomLogic 注册所有自定义逻辑
 func RegisterCustomLogic() {
-	// 注册私聊消息处理逻辑
+	//注册私聊消息处理逻辑
 	Manager.RegisterPrivateMessageHandler(func(client *client.QQClient, event *message.PrivateMessage) {
-		client.SendPrivateMessage(event.Sender.Uin, []message.IMessageElement{message.NewText("Hello World!")})
+		//client.SendPrivateMessage(event.Sender.Uin, []message.IMessageElement{message.NewText("Hello World!")})
+		logrus.Infof("[bot] message.private[uid:%v,msg:%v]", event.Sender.Uin, event.ToString())
 	})
-
-	// 注册群消息处理逻辑
 	Manager.RegisterGroupMessageHandler(func(client *client.QQClient, event *message.GroupMessage) {
-		client.SendGroupMessage(event.GroupUin, []message.IMessageElement{message.NewText("Hello World!")})
+		logrus.Infof("[bot] message.group[gid:%v,uid:%v,msg:%v]", event.GroupUin, event.Sender.Uin, event.ToString())
 	})
 
-	Manager.RegisterNewFriendRequestHandler(func(client *client.QQClient, event *event.NewFriendRequest) {
-		//event.SourceUid
-		logrus.Println("UID" + event.SourceUid)
-		client.SetFriendRequest(true, event.SourceUid)
+	Manager.RegisterGroupMessageHandler(func(client *client.QQClient, event *message.GroupMessage) {
+		msg := event.ToString()
+		if strings.HasPrefix(msg, "sgst") {
+			memberInfo := client.GetCachedMemberInfo(client.Uin, event.GroupUin)
+			if memberInfo.Permission == entity.Owner {
+				title := strings.TrimSpace(strings.TrimPrefix(msg, "sgst"))
+				_ = client.GroupSetSpecialTitle(event.GroupUin, event.Sender.Uin, title)
+			}
+		}
 	})
+
 }
 
 //
--- logic/logic.go
+++ logic/logic.go
@@ -8,18 +8,21 @@ import (
 )
 
 // 定义不同类型事件的处理函数类型
+
 type PrivateMessageHandler func(*client.QQClient, *message.PrivateMessage)
 type GroupMessageHandler func(*client.QQClient, *message.GroupMessage)
 type NewFriendRequestHandler func(*client.QQClient, *event.NewFriendRequest)
 
 // LogicManager 管理所有自定义逻辑
+
 type LogicManager struct {
-	privateMessageHandlers   []PrivateMessageHandler
-	groupMessageHandlers     []GroupMessageHandler
-	newFriendRequestHandlers []NewFriendRequestHandler
+	privateMessageHandlers []PrivateMessageHandler
+	groupMessageHandlers   []GroupMessageHandler
+	//newFriendRequestHandlers []NewFriendRequestHandler
 }
 
 // 全局 LogicManager 实例
+
 var Manager = &LogicManager{}
 
 // RegisterPrivateMessageHandler 注册私聊消息处理函数
@@ -30,11 +33,6 @@ func (lm *LogicManager) RegisterPrivateMessageHandler(handler PrivateMessageHand
 // RegisterGroupMessageHandler 注册群消息处理函数
 func (lm *LogicManager) RegisterGroupMessageHandler(handler GroupMessageHandler) {
 	lm.groupMessageHandlers = append(lm.groupMessageHandlers, handler)
-}
-
-// RegisterNewFriendRequestHandler 注册新朋友请求处理函数
-func (lm *LogicManager) RegisterNewFriendRequestHandler(handler NewFriendRequestHandler) {
-	lm.newFriendRequestHandlers = append(lm.newFriendRequestHandlers, handler)
 }
 
 // SetupLogic 设置所有逻辑处理
@@ -51,9 +49,10 @@ func SetupLogic() {
 		}
 	})
 
-	bot.QQClient.NewFriendRequestEvent.Subscribe(func(client *client.QQClient, event *event.NewFriendRequest) {
-		for _, handler := range Manager.newFriendRequestHandlers {
+	bot.QQClient.SelfGroupMessageEvent.Subscribe(func(client *client.QQClient, event *message.GroupMessage) {
+		for _, handler := range Manager.groupMessageHandlers {
 			handler(client, event)
 		}
 	})
+
 }
